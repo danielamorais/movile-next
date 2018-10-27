18f29fd33f5c32cebfc8f2e46b5b77fe5c2e88b9
Daniela Morais <Daniela Morais>
2018-10-27 16:44:09 -0300

    Add exemplos básicos de Kotlin

---
+++ MovileNext_Day1_Kotlin/app/src/main/java/com/example/office/movilenext_day1_kotlin/model/ProgrammingLanguage.kt
@@ -0,0 +1,8 @@
+package com.example.office.movilenext_day1_kotlin.model
+
+data class ProgrammingLanguage(
+    val imageResourceId: Int,
+    val title: String,
+    val year: Int,
+    val description: String
+)
