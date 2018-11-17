ba1598c3fc789420f8b39ffdbc86015157f412f2
Daniela Morais <Daniela Morais>
2018-11-17 15:05:47 -0200

    Add testes corretos com Robot Pattern

--- MovileNext_Day4_Robot/app/src/main/java/com/example/office/movilenext_day4_robot/LoginActivity.kt
+++ MovileNext_Day4_Robot/app/src/main/java/com/example/office/movilenext_day4_robot/LoginActivity.kt
@@ -3,6 +3,9 @@ package com.example.office.movilenext_day4_robot
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.yesButton
 
 class LoginActivity : AppCompatActivity() {
 
@@ -14,9 +17,24 @@ class LoginActivity : AppCompatActivity() {
             val email = etEmail.text.toString()
             val password = etPassword.text.toString()
 
-            if (email == "") {
-                tvNameSurname.text = getString(R.string.name_surname)
+            when {
+                emailIsValid(email, password) -> {
+                    tvNameSurname.text = getString(R.string.name_surname)
+                    longToast("Yay!!")
+                }
+                emailIsEmpty(email, password) -> alert(R.string.missing_fields) {
+                    yesButton { }
+                }.show()
+                else -> alert(R.string.login_fail) {
+                    yesButton { }
+                }.show()
             }
+
         }
     }
+
+    private fun emailIsEmpty(email: String, password: String) = email.isEmpty() || password.isEmpty()
+
+    private fun emailIsValid(email: String, password: String) =
+        email == "admin@admin.com" && password == "admin"
 }
